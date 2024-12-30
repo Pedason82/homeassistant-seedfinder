361fee5159392c51fc9c31e8694f78469589ac5c
dingausmwald <30699246+dingausmwald@users.noreply.github.com>
2024-12-30 13:08:50 +0100

    Update const.py

--- custom_components/openplantbook/const.py
+++ custom_components/openplantbook/const.py
@@ -8,17 +8,21 @@ ATTR_SPECIES = "species"
 ATTR_API = "api"
 ATTR_HOURS = "hours"
 ATTR_IMAGE = "image_url"
+ATTR_BREEDER = "breeder"
+ATTR_WEBSITE = "website"
+ATTR_INFOTEXT1 = "infotext1"
+ATTR_INFOTEXT2 = "infotext2"
+ATTR_EFFECTS = "effects"
+ATTR_SMELL = "smell"
+ATTR_TASTE = "taste"
+ATTR_LINEAGE = "lineage"
 CACHE_TIME = 24
 
-OPB_ATTR_SEARCH = "search"
-OPB_ATTR_SEARCH_RESULT = "search_result"
 OPB_ATTR_RESULT = "result"
 OPB_ATTR_RESULTS = "results"
 OPB_ATTR_TIMESTAMP = "timestamp"
 
-OPB_SERVICE_SEARCH = "search"
 OPB_SERVICE_GET = "get"
-OPB_SERVICE_UPLOAD = "upload"
 OPB_SERVICE_CLEAN_CACHE = "clean_cache"
 
 OPB_PID = "pid"
@@ -28,15 +32,5 @@ FLOW_DOWNLOAD_IMAGES = "download_images"
 FLOW_DOWNLOAD_PATH = "download_path"
 DEFAULT_IMAGE_PATH = "/config/www/images/plants/"
 
-OPB_MEASUREMENTS_TO_UPLOAD = [
-    "moisture",
-    "illuminance",
-    "conductivity",
-    "temperature",
-    "humidity",
-]
 OPB_INFO_MESSAGE = "info_message"
 OPB_CURRENT_INFO_MESSAGE = 1
-FLOW_UPLOAD_DATA = "upload_data"
-FLOW_UPLOAD_HASS_LOCATION_COUNTRY = "upload_data_hass_location_country"
-FLOW_UPLOAD_HASS_LOCATION_COORD = "upload_data_hass_location_coordinates"
